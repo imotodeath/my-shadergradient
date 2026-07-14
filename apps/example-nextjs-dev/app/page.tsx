738d933c1fdbb3a7b268b6081c0b91f68c08bf24
imotodeath <hirokideath@gmail.com>
2026-07-14 14:15:43 +0900

    shaderの調整

--- apps/example-nextjs-dev/app/page.tsx
+++ apps/example-nextjs-dev/app/page.tsx
@@ -15,40 +15,48 @@ export default function Home() {
       }}
     >
       <ShaderGradient
-        animate="on"
-        brightness={1.1}
-        cAzimuthAngle={180}
-        cDistance={3.9}
-        cPolarAngle={115}
-        cameraZoom={1}
-        color1="#5606ff"
-        color2="#fe8989"
-        color3="#000000"
-        envPreset="city"
-        fov={45}
-        grain="off"
-        lightType="3d"
-        pixelDensity={1}
-        positionX={-0.5}
-        positionY={0.1}
-        positionZ={0}
-        range="disabled"
-        rangeEnd={40}
-        rangeStart={0}
-        reflection={0.1}
-        rotationX={0}
-        rotationY={0}
-        rotationZ={235}
-        shader="defaults"
-        type="waterPlane"
-        uAmplitude={0}
-        uDensity={1.1}
-        uFrequency={5.5}
-        uSpeed={0.1}
-        uStrength={2.4}
-        uTime={0.2}
-        wireframe={false}
-      />
+  animate="on"
+  axesHelper="off"
+  bgColor1="#000000"
+  bgColor2="#000000"
+  brightness={1.3}
+  cAzimuthAngle={170}
+  cDistance={4.4}
+  cPolarAngle={70}
+  cameraZoom={1}
+  color1="#fcff2e"
+  color2="#7af0ff"
+  color3="#ffffff"
+  destination="onCanvas"
+  embedMode="off"
+  envPreset="city"
+  format="gif"
+  fov={45}
+  frameRate={10}
+  gizmoHelper="hide"
+  grain="off"
+  lightType="3d"
+  pixelDensity={1}
+  positionX={0}
+  positionY={0.9}
+  positionZ={-0.3}
+  range="enabled"
+  rangeEnd={40}
+  rangeStart={0}
+  reflection={0.1}
+  rotationX={45}
+  rotationY={0}
+  rotationZ={0}
+  shader="defaults"
+  type="sphere"
+  uAmplitude={5.9}
+  uDensity={1.3}
+  uFrequency={0}
+  uSpeed={0.05}
+  uStrength={7.5}
+  uTime={0}
+  wireframe={false}
+/>
     </ShaderGradientCanvas>
   )
 }
