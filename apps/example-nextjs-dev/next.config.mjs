4a2d35ad7a001c841d2a2b387784a0c8cc4763b4
井本大樹 <imotohiroki@imotodaikinoMac-mini.local>
2026-07-14 13:47:13 +0900

    ビルドコマンドの調整とパス変更

--- apps/example-nextjs-dev/next.config.mjs
+++ apps/example-nextjs-dev/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // ← この行を新しく追加します
+  // すでに他の設定が書いてある場合は、その下にカンマ（,）で区切って続けてください
+};
 
 export default nextConfig;
